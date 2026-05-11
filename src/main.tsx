a91a0b45e3709f448d215ed77297d2b2727bc65d
dedalo-integraciones <dedalo.integraciones@gmail.com>
2026-05-11 12:20:04 -0300

    Remove powered-by image from main component

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
-      <img src="https://i.ibb.co/FLTMwg1n/powered-by-Negro.png" alt="Powered By" className="fixed bottom-3 right-4 z-[9999] h-[24px] w-auto opacity-70 pointer-events-none" />
     </ErrorBoundary>
   </StrictMode>,
 );
